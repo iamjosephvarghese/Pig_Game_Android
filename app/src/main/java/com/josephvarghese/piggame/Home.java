5311957cc5349ff8f52ed45ca9f455181c9e5689
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-11-23 12:21:46 +0530

    added ui

--- app/src/main/java/com/josephvarghese/piggame/Home.java
+++ app/src/main/java/com/josephvarghese/piggame/Home.java
@@ -2,6 +2,7 @@ package com.josephvarghese.piggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Log.d("Inside","Homeactiity");
     }
 }
