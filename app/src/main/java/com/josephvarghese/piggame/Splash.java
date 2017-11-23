f4fd19b0dcb62877b5b45eb9987003b7ac4e9a70
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-11-23 11:18:58 +0530

    added runnable in splash screen

--- app/src/main/java/com/josephvarghese/piggame/Splash.java
+++ app/src/main/java/com/josephvarghese/piggame/Splash.java
@@ -1,7 +1,9 @@
 package com.josephvarghese.piggame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                Intent homeIntent = new Intent(Splash.this,Home.class);
+                startActivity(homeIntent);
+
+            }
+        },300);
     }
 }
